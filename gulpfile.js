c380e05ea66ff714aad9cbcc288505b3c6f99a11
Feb <dinhhien102@gmail.com>
2018-09-26 11:17:42 +0700

    Get data from json file

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
+'use strict';
+
 var gulp = require('gulp'),
     autoprefixer = require('gulp-autoprefixer'),
     sass = require('gulp-sass'),
+    twig = require('gulp-twig'),
+    data = require('gulp-data'),
+    path = require('path'),
+    prettify = require('gulp-html-prettify'),
     browserSync = require('browser-sync').create();
 
 gulp.task('default', function(){
@@ -15,7 +21,7 @@ gulp.task('prefix', function(){
     .pipe(gulp.dest('dist'))
 });
 
-gulp.task('sass', function () {
+gulp.task('sass', function(){
   return gulp.src('./scss/**/*.scss')
     .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
     .pipe(autoprefixer({
@@ -30,5 +36,19 @@ gulp.task('watch', function () {
     server: "./app"
   });
   gulp.watch('./scss/**/*.scss', ['sass']);
+  gulp.watch('**/*.twig', ['twig']);
   gulp.watch('./app/**/*.html').on('change', browserSync.reload);
 });
+
+var getJsonData = function(file) {
+  return require('./' + path.basename(file.path, '.twig') + '.json');
+};
+
+gulp.task('twig', function(){
+  var twig = require('gulp-twig');
+  return gulp.src('./index.twig')
+    .pipe(data(getJsonData))
+    .pipe(twig())
+    .pipe(prettify({indent_char: ' ', indent_size: 2}))
+    .pipe(gulp.dest('./app'));
+})
